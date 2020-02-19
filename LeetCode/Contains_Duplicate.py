616b899c15a8782497cce46918a6181e9a43fe8f
Yiğit Varlı <44034966+yigitv4rli@users.noreply.github.com>
2020-02-19 15:04:56 +0300

    Create Contains_Duplicate.py

---
+++ LeetCode/Contains_Duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic = {}
+        for num in nums:
+            if num not in dic.keys():
+                dic[num] = 1
+            else:
+                return True
+        return False 
